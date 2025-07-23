e990b7d157ebf70360775f1c8fb304eb010a38a6
Shaeel Shahid <135009664+Shaeel-0005@users.noreply.github.com>
2025-07-24 00:30:49 +0500

    Project setup

---
+++ .history/globetrek/src/App_20250723201509.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gradient-to-br from-blue-400 to-purple-500">
+      <h1 className="text-4xl font-bold text-white">GlobeTrek 🌍 is Live!</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ .history/globetrek/src/App_20250723201517.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css'
+
+
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gradient-to-br from-blue-400 to-purple-500">
+      <h1 className="text-4xl font-bold text-white">GlobeTrek 🌍 is Live!</h1>
+    </div>
+  );
+}
+
+export default App;
