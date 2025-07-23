4908f1a1434032ba7fee2b502a8d93802426940d
Shaeel Shahid <135009664+Shaeel-0005@users.noreply.github.com>
2025-07-24 00:34:13 +0500

    testing app runing

--- .history/globetrek/src/App_20250724003358.jsx
+++ .history/globetrek/src/App_20250724003358.jsx
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="min-h-screen flex items-center justify-center bg-gradient-to-br from-blue-400 to-purple-500">
       <h1 className="text-4xl font-bold text-white">GlobeTrek 🌍 is Live!</h1>
-      <p>something</p>
+      <p></p>
     </div>
   );
 }
