60369aaa2669652d077844e500e29a1c62dca85a
Shaeel Shahid <135009664+Shaeel-0005@users.noreply.github.com>
2026-01-26 11:02:10 +0500

    Update Supabase URL and anon key

--- globetrek/src/supabase.js
+++ globetrek/src/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://itoosdrmynwaemptstel.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Iml0b29zZHJteW53YWVtcHRzdGVsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTU3Nzc0MjksImV4cCI6MjA3MTM1MzQyOX0.rgHZYedQNQ5bd5zK7PjgxZyxHW5Y59haZ8YRct00w_o';
+const supabaseUrl = 'https://pxgqvfibmvcpuunltzkm.supabase.co';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB4Z3F2ZmlibXZjcHV1bmx0emttIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njk0MDE1NDksImV4cCI6MjA4NDk3NzU0OX0.XMXau4ZCIFAgwy2BeXxQFF-pmOdvx9yHYi5w1o2HnQA';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
