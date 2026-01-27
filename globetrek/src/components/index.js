6d5f38f3b18f714f21c6e951841e94d9633ab738
Shaeel Shahid <135009664+Shaeel-0005@users.noreply.github.com>
2026-01-27 15:49:18 +0500

    Add LocationPicker component to exports

--- globetrek/src/components/index.js
+++ globetrek/src/components/index.js
@@ -1,9 +1,11 @@
 import Navbar from "./navbar";
 import AddJournal from "./addjournal";
 import AuthCallback from "./authcallback"
+import LocationPicker from "./locationPicker"
 
 export { 
     Navbar,
     AddJournal,
-    AuthCallback
+    AuthCallback,
+    
  };
